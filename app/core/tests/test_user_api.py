7b776440ada9b5acd4a2adcee0e6c860a31d0d12
baris <baris@heartsapiens.art>
2025-02-06 21:52:47 +0300

    Implemented user API. (And URL extension in the test_user_api.py)

--- app/core/tests/test_user_api.py
+++ app/core/tests/test_user_api.py
@@ -9,7 +9,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 
-CREATE_USER_URL = reverse('user:user-create')
+CREATE_USER_URL = reverse('user:create')
 
 
 def create_user(**params):
